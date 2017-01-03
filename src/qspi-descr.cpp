8cb530012ffe30fbf9eb1d307902b1a189bad18c
Lix N. Paulian <lix@paulian.net>
2017-01-03 18:27:21 +0100

    Flash description structures and tables for several devices added.

---
+++ src/qspi-descr.cpp
@@ -0,0 +1,61 @@
+/*
+ * qspi-descr.cpp
+ *
+ * Copyright (c) 2017 Lix N. Paulian (lix@paulian.net)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Created on: 1 Jan 2017 (LNP)
+ */
+
+#include "qspi-descr.h"
+
+// Micron devices
+const qspi_device_t micron_devices[] =
+  {
+    { 0xBA18, 4096, "MT25QL128ABA", true, false },
+    { 0xBB18, 4096, "MT25QL128ABA", true, false },
+    { } //
+  };
+
+// Winbond devices
+const qspi_device_t winbond_devices[] =
+  {
+    { 0x4015, 4096, "W25Q16DV", false, true },
+    { 0x4016, 4096, "W25Q32FV", false, true },
+    { 0x6016, 4096, "W25Q32FV", false, true },
+    { 0x4017, 4096, "W25Q64FV", false, true },
+    { 0x6017, 4096, "W25Q64FV", false, true },
+    { 0x4018, 4096, "W25Q128FV", false, true },
+    { 0x6018, 4096, "W25Q128FV", false, true },
+    { 0x7018, 4096, "W25Q128JV", true, true },
+    { } //
+  };
+
+// Supported manufactures
+const qspi_manuf_t qspi_manufacturers[] =
+  {
+    { MANUF_ID_MICRON, "Micron (ST)", micron_devices },
+    { MANUF_ID_WINBOND, "Winbond", winbond_devices },
+    { } //
+  };
+
---
+++ src/qspi-descr.h
@@ -0,0 +1,56 @@
+/*
+ * qspi-descr.h
+ *
+ * Copyright (c) 2017 Lix N. Paulian (lix@paulian.net)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Created on: 1 Jan 2017 (LNP)
+ */
+
+#ifndef QSPI_DESCR_H_
+#define QSPI_DESCR_H_
+
+#include "stdint.h"
+
+#define MANUF_ID_MICRON 0x20
+#define MANUF_ID_WINBOND 0xEF
+
+typedef struct qspi_device_s
+{
+  uint16_t device_ID;
+  uint16_t sector_size;
+  const char* device_name;
+  bool DTR_support;
+  bool pwrdown_support;
+} qspi_device_t;
+
+typedef struct qspi_manuf_s
+{
+  uint8_t manufacturer_ID;
+  const char* manufacturer_name;
+  const qspi_device_t* devices;
+} qspi_manuf_t;
+
+extern const qspi_manuf_t qspi_manufacturers[];
+
+#endif /* QSPI_DESCR_H_ */
